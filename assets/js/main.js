cb52b34b376200c1abd22aed44c491e9d7edc99d
dmobrien <dmobrienllc@gmail.com>
2021-06-17 14:24:27 -0500

    Getting closer! Bug resulting in reset not working correctly.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,21 @@ let startButton = document.querySelector(".start-button");
 let timer;
 let timerCount;
 
+let gamesCounter = 0;
 let questionsCounter = 0;
 let correctCounter = 0;
 let incorrectCounter = 0;
 let stopTimer = false
+
+class Game{
+  constructor(index, dateTime,numCorrect,numIncorrect,initials) {
+    this.index = index;
+    this.dateTime = dateTime;
+    this.numCorrect = numCorrect;
+    this.numIncorrect = numIncorrect;
+    this.initials = initials;
+  }
+}
 
 class Question {
   constructor(index, questionText,correctAnswerIndex) {
@@ -45,7 +56,7 @@ function init() {
 
 //start button onclick
 function startGame() {
-  timerCount = 60;
+  timerCount = 30;
   startButton.disabled = true;
 
   startTimer()
@@ -54,13 +65,18 @@ function startGame() {
   renderQuestion(0);
 }
 
+function stopGame(){
+  
+}
+
 function startTimer() {
 
   timer = setInterval(function() {
     timerCount--;
     timerCnt.textContent = timerCount;
 
-    //reset button/processAnswer methods can both set stopTimer to true
+    //processAnswer method set stopTimer to true
+    //if no more questions.
     if (timerCount === 0 || stopTimer) {
       clearInterval(timer); 
     }
@@ -87,10 +103,11 @@ function renderQuestion(questionIndex) {
       buttonEl.setAttribute("class","answer-button");
       buttonEl.setAttribute("id",index);
       buttonEl.setAttribute("value",answer.answerText);
+      buttonEl.setAttribute("data-correct-answer",answer.isCorrect);
       buttonEl.innerText = answer.answerText;
 
       buttonEl.addEventListener("click", function() {
-            processAnswer(questTmp,index);
+            processAnswer(questTmp,answer);
         });
       liEl.appendChild(buttonEl);
       ulEl.appendChild(liEl);
@@ -98,19 +115,18 @@ function renderQuestion(questionIndex) {
 }
 
 //process the answer function
-function processAnswer(questTmp,answerIndex){
-  alert("QuestionIndex: " + questTmp.index + "You answered: " + answerIndex)
+function processAnswer(questTmp,answerTmp){
+  console.log("ProcessAnswerTmpQst " + questTmp.correctAnswerIndex + " | Selected:" + answerTmp.index);
 
-  console.log("ProcessAnswerTmpQst " + questTmp.correctAnswerIndex + " | Selected:" + answerIndex);
-
-  //Return message should be human friendly. write this to p tag
-  if(SetCorrectIncorrect(questTmp.correctAnswerIndex,answerIndex)){
-    alert(questTmp.correctAnswerIndex + "| " + answerIndex 
+  if(SetCorrectIncorrect(answerTmp.isCorrect)){
+    console.log(questTmp.correctAnswerIndex + "| " + answerTmp.index 
       + " " + questTmp.questionText + " answered correctly!")
+      alert("Correct!");
   }
   else{
-    alert(questTmp.correctAnswerIndex + "| " + answerIndex + " " 
+    console.log(questTmp.correctAnswerIndex + "| " + answerTmp.index + " " 
       + questTmp.questionText + " answered incorrectly!")
+      alert("Incorrect!");
   }
 
   let questionIndex = questTmp.index++;
@@ -124,8 +140,8 @@ function processAnswer(questTmp,answerIndex){
   }
 }
 
-function SetCorrectIncorrect(correctIndex,chosenIndex){
-  if(correctIndex === chosenIndex){
+function SetCorrectIncorrect(isCorrect){
+  if(isCorrect){
     correctCounter++;
     correctCnt.innerText = correctCounter;
     localStorage.setItem("correctAnswers",correctCounter);
@@ -133,12 +149,12 @@ function SetCorrectIncorrect(correctIndex,chosenIndex){
   }
   else{
     incorrectCounter++;
-    alert("Set incorrect " + incorrectCounter);
     localStorage.setItem("incorrectAnswers",incorrectCounter)
     incorrectCnt.innerText = incorrectCounter;
     return false;
   }
 }
+
 //put together meaningful results based on correct/incorrect
 function displayResults()
 {
@@ -149,10 +165,12 @@ function displayResults()
   let correctCnt = localStorage.getItem("correctAnswers");
   let incorrectCnt = localStorage.getItem("incorrectAnswers");
 
-  alert("Display: No More Questions! Correct: " +
+  console.log("Display: No More Questions! Correct: " +
       correctCnt + " Incorrect: " + incorrectCnt);
 
-  if(correctCnt > incorrectCnt){
+  if(correctCnt === 0){
+    pQuestionEl.innerText = "How could you not answer a single question?!?!"
+  }else if(correctCnt > incorrectCnt){
     pQuestionEl.innerText = "There's no question, you've defeated the quiz!!"
   }else if(correctCnt < incorrectCnt){
     pQuestionEl.innerText = "There's no question, you've been defeated by the quiz!!"
@@ -169,35 +187,16 @@ function displayResults()
   }
 }
 
-  function getCorrectAnswers(){
-    // Get stored value from client storage, if it exists
-    var storedCorrect = localStorage.getItem("correctCount");
-    // If stored value doesn't exist, set counter to 0
-    if (storedCorrect === null) {
-      correctCounter = 0;
-    } else {
-      // If a value is retrieved from client storage set the winCounter to that value
-      correctCounter = storedCorrect;
-    }
-      correctCnt.textContent = correctCounter;
-    }
-
-  function getIncorrectAnswers(){
-    // Get stored value from client storage, if it exists
-    var storedIncorrect = localStorage.getItem("incorrectCount");
-    // If stored value doesn't exist, set counter to 0
-    if (storedIncorrect === null) {
-      incorrectCounter = 0;
-    } else {
-      // If a value is retrieved from client storage set the winCounter to that value
-      incorrectCounter = storedIncorrect;
-    }
-      incorrectCnt.textContent = incorrectCounter;
-  }
-
   //This function to be refactored to call a 'create' constructor to
   //avoid duplication
   function loadQuestions(){
+
+    //ensure we do a fresh load
+    if(questionsArray.length>0)
+    {
+      questionsArray.length = 0;
+    }
+
     let testQuestion = "Arrays in javascript can be used to hold __________?";
     let answers = ["boolean","string","object","all of the above"];
     let question = new Question(1,testQuestion,3);
@@ -256,15 +255,106 @@ init();
 var resetButton = document.querySelector(".reset-button");
 
 function resetGame() {
-
-   correctCounter = 0;
-   incorrectCounter = 0;
-   // Renders correct/incorrect counts and sets them into client storage
-   //in form of a game object
-   //setGameMethod wojuld be enough
-   stopTimer = true;
-   setCorrect()
-   setIncorrect()
+  correctCounter = 0;
+  incorrectCounter = 0;
+  stopTimer = false;
+  timerCnt.innerText = 30;
+  correctCnt.innerText = 0;
+  incorrectCnt.innerText = 0;
+  startButton.disabled = false;
+  localStorage.setItem("correctCount","");
+  localStorage.setItem("incorrectCount","");
 }
 // Attaches event listener to button
 resetButton.addEventListener("click", resetGame);
+
+
+
+// function SetCorrectIncorrect(correctIndex,chosenIndex){
+//   if(correctIndex === chosenIndex){
+//     correctCounter++;
+//     correctCnt.innerText = correctCounter;
+//     localStorage.setItem("correctAnswers",correctCounter);
+//     return true;
+//   }
+//   else{
+//     incorrectCounter++;
+//     alert("Set incorrect " + incorrectCounter);
+//     localStorage.setItem("incorrectAnswers",incorrectCounter)
+//     incorrectCnt.innerText = incorrectCounter;
+//     return false;
+//   }
+// }
+
+// function getCorrectAnswers(){
+//   // Get stored value from client storage, if it exists
+//   var storedCorrect = localStorage.getItem("correctCount");
+//   // If stored value doesn't exist, set counter to 0
+//   if (storedCorrect === null) {
+//     correctCounter = 0;
+//   } else {
+//     // If a value is retrieved from client storage set the winCounter to that value
+//     correctCounter = storedCorrect;
+//   }
+//     correctCnt.textContent = correctCounter;
+//   }
+
+// function getIncorrectAnswers(){
+//   // Get stored value from client storage, if it exists
+//   var storedIncorrect = localStorage.getItem("incorrectCount");
+//   // If stored value doesn't exist, set counter to 0
+//   if (storedIncorrect === null) {
+//     incorrectCounter = 0;
+//   } else {
+//     // If a value is retrieved from client storage set the winCounter to that value
+//     incorrectCounter = storedIncorrect;
+//   }
+//     incorrectCnt.textContent = incorrectCounter;
+// }
+
+// var container = document.querySelector(".container");
+// var para = document.querySelector("#result");
+
+// container.addEventListener("click", function(event) {
+//   var element = event.target;
+//   console.log(element);
+
+//   var state = element.getAttribute("data-state");
+//   var number = element.getAttribute("data-number");
+
+//   "[id='22']"
+//   var selectionAttString = "[data-number='" + number + "']";
+
+//   var divEl = document.querySelector(selectionAttString);
+//   divEl.innerHTML = number;
+  
+//   element.setAttribute("data-state", "visible");
+
+//   para.innerHTML= state + number;
+// });
+
+// function processAnswer(questTmp,answerIndex){
+//   alert("QuestionIndex: " + questTmp.index + "You answered: " + answerIndex)
+
+//   console.log("ProcessAnswerTmpQst " + questTmp.correctAnswerIndex + " | Selected:" + answerIndex);
+
+//   //Return message should be human friendly. write this to p tag
+//   if(SetCorrectIncorrect(questTmp.correctAnswerIndex,answerIndex)){
+//     alert(questTmp.correctAnswerIndex + "| " + answerIndex 
+//       + " " + questTmp.questionText + " answered correctly!")
+//   }
+//   else{
+//     alert(questTmp.correctAnswerIndex + "| " + answerIndex + " " 
+//       + questTmp.questionText + " answered incorrectly!")
+//   }
+
+//   let questionIndex = questTmp.index++;
+
+//   //get next question or end if no  more questions
+//   if(questionIndex > (questionsArray.length - 1)){
+//     displayResults();
+//   }
+//   else{
+//     renderQuestion(questionIndex);
+//   }
+// }
